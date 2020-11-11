15b202a7a49ef3d9f19fa798cd3522005cfa5038
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2020-11-11 18:45:27 +0100

    register service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
+import { RegisterServiceWorker } from './RegisterServiceWorker';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Boilerplate - PWA
-        </p>
-        <p>
-          Version 1
-        </p>
-      </header>
-    </div>
+    <RegisterServiceWorker>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Boilerplate - PWA
+          </p>
+          <p>
+            Version 1
+          </p>
+        </header>
+      </div>
+    </RegisterServiceWorker>
   );
 }
 
