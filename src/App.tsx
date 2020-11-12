293534a75156dc55daa2f1927c8480fe0d38350b
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2020-11-12 12:35:43 +0100

    try default option from CRA

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import { RegisterServiceWorker } from './RegisterServiceWorker';
 
 function App() {
   return (
     <>
-      <RegisterServiceWorker />
+      {/* <RegisterServiceWorker /> */}
       <div className="App">
         <header className="App-header">
           <p>
