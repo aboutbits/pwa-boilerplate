9833d0948d1a3389f22d3ecf582316ff27867fb9
Natan Cieplinski <natan.cieplinski.developer@gmail.com>
2021-03-12 15:57:25 +0100

    Lint fixes

--- src/service-worker-noop.js
+++ src/service-worker-noop.js
@@ -3,22 +3,22 @@
 
 // This line is required by Create React App Webpack configuration, but it has no effect in this case.
 // eslint-disable-next-line no-unused-vars
-const ignored = self.__WB_MANIFEST;
+const ignored = self.__WB_MANIFEST
 
-self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('install', () => self.skipWaiting())
 
-self.addEventListener('activate', event => {
-  event.waitUntil(burnDownTheHouse());
-});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(burnDownTheHouse())
+})
 
 async function burnDownTheHouse() {
   // Unregister
-  self.registration.unregister();
+  self.registration.unregister()
   // Delete all caches
-  const keys = await self.caches.keys();
-  await Promise.all(keys.map(key => self.caches.delete(key)));
+  const keys = await self.caches.keys()
+  await Promise.all(keys.map((key) => self.caches.delete(key)))
   // Force refresh all windows
-  const clients = await self.clients.matchAll({ type: 'window' });
+  const clients = await self.clients.matchAll({ type: 'window' })
   clients.forEach((client) => {
     if ('navigate' in client) {
       client.navigate(client.url)
