76bc4d4a2269e9d106014dcc24c1e15bcd2f9e6e
Martin Malfertheiner <martin.malfertheiner@aboutbits.it>
2021-03-17 11:29:15 +0100

    Revert "Add push notification"
    This reverts commit 1f1c5b710a291fc90551f462e12dd71da661da86.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import './App.css'
-import { getToken, onMessageListener } from './firebase'
 
 function App() {
-  getToken()
-  onMessageListener()
-    .then((payload) => {
-      //setNotification({title: payload.notification.title, body: payload.notification.body})
-      console.log(payload)
-    })
-    .catch((err) => console.log('failed: ', err))
-
   return (
     <div className="App">
       <header className="App-header">
         <p>Boilerplate - PWA</p>
-        <p>Version 23</p>
+        <p>Version 20</p>
       </header>
     </div>
   )
